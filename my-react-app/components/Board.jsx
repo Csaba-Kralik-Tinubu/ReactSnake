20c12ffa8f0ab76278c2b5f6d349a1d422561aae
Csaba-Kralik <csaba.kralik@innoveo.com>
2026-04-20 15:08:35 +0200

    add working board

--- my-react-app/components/Board.jsx
+++ my-react-app/components/Board.jsx
@@ -7,15 +7,17 @@ function Board(props){
  const height = props. height;
 
 const mystyle = {
-x: positionX,
-y: positionY,
-width: width,
-height: height,
+"x": positionX,
+"y": positionY,
+"width": width,
+"height": height,
+"background-color": "green",
+"position": "absolute"
 }
 
 return (
-    <div style="width:500px, height:600px, background-color:green">
-    
+    <div style={mystyle}>
+    <p>hello</p>
     </div>
 )
 }
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className='App'>
-      <Board positionX={100} positionY={100} width={400} height={400} />
+      <Board positionX={400} positionY={400} width={400} height={400} />
     </div>
   )
 }

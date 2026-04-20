25cacc6ad5516bf25ac30d5dbb3c7fc916d94b33
Olivér Simon <oliver.simon@tinubu.com>
2026-04-20 15:14:21 +0200

    remove snake useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Board from '../components/Board'
+import SnakeHead from '../components/SnakeHead'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Board positionX={400} positionY={400} width={400} height={400} />
+      <SnakeHead />
     </div>
   )
 }

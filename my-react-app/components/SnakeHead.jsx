a587a43cc6ac953efc749ed4338b1d15697c2b1c
Olivér Simon <oliver.simon@tinubu.com>
2026-04-20 14:09:31 +0200

    add snake component basic

---
+++ my-react-app/components/SnakeHead.jsx
@@ -0,0 +1,11 @@
+function SnakeHead (props) {
+    const [snakeX, setSnakeX] = useState(0);
+    const [snakeY, setSnakeY] = useState(0);
+
+
+    return (
+        <div>O</div>
+    );
+}
+
+export default SnakeHead;
